9dce2ebb8c294c36ba131763f422835d58220863
biffyn <james@jamesrichards.dev>
2020-08-17 22:31:43 +0100

    fix object is null error and update tests

--- packages/nx-hugo/src/schematics/init/schematic.spec.ts
+++ packages/nx-hugo/src/schematics/init/schematic.spec.ts
@@ -1,18 +1,53 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { createEmptyWorkspace } from '@nrwl/workspace/testing';
-import { join } from 'path'
 import { readJsonInTree } from '@nrwl/workspace';
 
-import { InitSchematicSchema } from './schema';
 import { runSchematic } from '../../utils/testing';
 
 describe('init schematic', () => {
   let appTree: Tree;
-  const options: InitSchematicSchema = { name: 'test',  };
+  const prettierOverrides = [
+    {
+      files: ['*.html'],
+      options: {
+        parser: 'go-template',
+      },
+    },
+  ]
 
-  beforeEach(() => {
-    appTree = createEmptyWorkspace(Tree.empty());
+  beforeEach(() => {	
+    appTree = createEmptyWorkspace(Tree.empty());	    
+  });	  
+
+  it('should run successfully', async () => {	
+    await expect(runSchematic(
+      'init', { name: 'test' }, appTree)
+      ).resolves.not.toThrowError();
+  });
+
+  it('should add dependencies', async () => {	
+    const result = await runSchematic('init', { name: 'test' }, appTree);
+    const packageJson = readJsonInTree(result, 'package.json');
+
+    expect(packageJson.devDependencies['hugo-bin']).toBeDefined();
+    expect(packageJson.devDependencies['@nrwl/workspace']).toBeDefined();
+    expect(packageJson.devDependencies['prettier-plugin-go-template']).toBeDefined();
+    expect(packageJson.devDependencies['postcss-cli']).toBeDefined();
+    expect(packageJson.devDependencies['autoprefixer']).toBeDefined();
+  });
+
+  it('should add hugo etended', async () => {	
+    const result = await runSchematic('init', { name: 'test', extended: true }, appTree);
+    const packageJson = readJsonInTree(result, 'package.json');
+
+    expect(packageJson['hugo-bin'].buildTags).toEqual('extended');
+  });
+
+  it('should upadte prettierrc', async () => {	
+    const result = await runSchematic('init', { name: 'test' }, appTree);
+    const prettierrc = readJsonInTree(result, '.prettierrc');
+
+    expect(prettierrc.overrides).toEqual(prettierOverrides);
   });
 
 });
