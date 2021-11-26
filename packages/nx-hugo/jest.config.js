b311b929b6cdf2a565117dacef68d0bdec2eae71
James Richards <james@jamesrichards.dev>
2021-11-26 23:01:34 +0000

    updare nx workspace

--- packages/nx-hugo/jest.config.js
+++ packages/nx-hugo/jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  name: 'nx-hugo',
-  preset: '../../jest.config.js',
+  preset: '../../jest.preset.js',
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
   coverageDirectory: '../../coverage/packages/nx-hugo',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
+  displayName: 'nx-hugo',
+  testEnvironment: 'node',
 };
